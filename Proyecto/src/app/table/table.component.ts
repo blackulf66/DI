eeef0455ab8dfa8a01a5901160eb953c355f2cd9
Francisco Javier Navarro Vega <navarro.vefra21@triana.salesianos.edu>
2021-10-14 09:01:41 +0200

    botones v2

--- Proyecto/src/app/table/table.component.ts
+++ Proyecto/src/app/table/table.component.ts
@@ -3,23 +3,22 @@ import { Component, OnInit } from '@angular/core';
 export interface elemento {
   position: number;
   nombre: string;
-  apellidos: String;  
+  apellidos: String;
   edad: number;
   curso: number;
   notamedia: number;
-  boton1:String;
-  boton2:String;
+
 
 }
-const ELEMENT_DATA: elemento[] = [
-  {position: 1, nombre: 'sven', apellidos:'svensson', edad: 2, curso:1, notamedia: 2, boton1:'editar',boton2:'eliminar'},
-  {position: 2, nombre: 'sven', apellidos:'svensson', edad: 2, curso:1, notamedia: 2, boton1:'editar',boton2:'eliminar'},
-  {position: 3, nombre: 'sven', apellidos:'svensson' ,edad: 2, curso:1 ,notamedia: 2, boton1:'editar',boton2:'eliminar'},
-  {position: 4, nombre: 'sven', apellidos:'svensson' ,edad: 2, curso:1 ,notamedia: 2, boton1:'editar',boton2:'eliminar'},
-  {position: 5, nombre: 'sven', apellidos:'svensson' ,edad: 2, curso:1 ,notamedia: 2, boton1:'editar',boton2:'eliminar'},
-  {position: 6, nombre: 'sven', apellidos:'svensson' ,edad: 2, curso:1, notamedia: 2, boton1:'editar',boton2:'eliminar'},
-  {position: 7, nombre: 'sven', apellidos:'svensson', edad: 2, curso:1 ,notamedia: 2, boton1:'editar',boton2:'eliminar'},
-  {position: 8, nombre: 'sven', apellidos:'svensson' ,edad: 2, curso:1 ,notamedia: 2, boton1:'editar',boton2:'eliminar'},
+const ELEMENTOS: elemento[] = [
+  { position: 1, nombre: 'sven', apellidos: 'svensson', edad: 2, curso: 1, notamedia: 2 },
+  { position: 2, nombre: 'sven', apellidos: 'svensson', edad: 2, curso: 1, notamedia: 2 },
+  { position: 3, nombre: 'sven', apellidos: 'svensson', edad: 2, curso: 1, notamedia: 2 },
+  { position: 4, nombre: 'sven', apellidos: 'svensson', edad: 2, curso: 1, notamedia: 2 },
+  { position: 5, nombre: 'sven', apellidos: 'svensson', edad: 2, curso: 1, notamedia: 2 },
+  { position: 6, nombre: 'sven', apellidos: 'svensson', edad: 2, curso: 1, notamedia: 2 },
+
+
 ];
 @Component({
   selector: 'app-table',
@@ -28,8 +27,8 @@ const ELEMENT_DATA: elemento[] = [
 })
 
 export class TableComponent implements OnInit {
-  displayedColumns: string[] = ['position','nombre','apellidos','edad','curso','nota','boton1','boton2'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['position', 'nombre', 'apellidos', 'edad', 'curso', 'nota', 'acciones'];
+  dataSource = ELEMENTOS;
 
   constructor() { }
 
