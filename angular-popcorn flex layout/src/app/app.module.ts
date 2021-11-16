8f82759ac2640ffa3b000a3b527562c6b662379b
Francisco Javier Navarro Vega <navarro.vefra21@triana.salesianos.edu>
2021-11-16 13:38:46 +0100

    añadido el layout

---
+++ angular-popcorn flex layout/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialImportsModule } from './modules/material-imports.module';
+import { MoviesPopularListComponent } from './components/movies-popular-list/movies-popular-list.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MoviesPopularListComponent,
+    MovieItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    NgCircleProgressModule.forRoot({
+  
+      radius: 100,
+      outerStrokeWidth: 10,
+      innerStrokeWidth: 5,
+      outerStrokeColor: "#8F0091",
+      innerStrokeColor: "#CE00D1",
+      animationDuration: 300,
+      titleColor: "#FFFFFF"
+    })
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
