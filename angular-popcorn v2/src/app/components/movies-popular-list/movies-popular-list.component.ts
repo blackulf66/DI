c2b802b1ffedeb978e3b5c3ca5b8772fd4258262
Francisco Javier Navarro Vega <navarro.vefra21@triana.salesianos.edu>
2021-11-23 13:05:40 +0100

    filtros complejos añadidos

--- angular-popcorn v2/src/app/components/movies-popular-list/movies-popular-list.component.ts
+++ angular-popcorn v2/src/app/components/movies-popular-list/movies-popular-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Genre, GenreList } from 'src/app/models/interfaces/genre.interface';
 import { Movie } from 'src/app/models/interfaces/movies-popular.interface';
 import { AuthService } from 'src/app/services/auth.service';
 import { MoviesService } from 'src/app/services/movies.service';
@@ -10,15 +11,33 @@ import { MoviesService } from 'src/app/services/movies.service';
 })
 export class MoviesPopularListComponent implements OnInit {
   popularMovies: Movie[] = [];
+  allPopularMovies: Movie[] = [];
+  genreList!: Genre[];
+  genreid!: number[];
 
   constructor(private moviesService: MoviesService,
-    private authService: AuthService
+    private authService: AuthService,
+  
     ) { }
 
   ngOnInit(): void {
     this.moviesService.getPopularMovies().subscribe(popularMoviesResponse => {
       this.popularMovies = popularMoviesResponse.results;
+      this.allPopularMovies = popularMoviesResponse.results;
     });
+    this.moviesService.getGenreList().subscribe(g => {
+      this.genreList = g.genres;
+    })
   }
+  filterGenre(sGeneroid: number[]){
 
+      this.popularMovies = this.allPopularMovies;
+      if(this.genreid.length > 0){
+        let r :Movie[] = this.popularMovies.filter(movies => movies.genre_ids.some(filtro => sGeneroid.includes(filtro)));
+      this.popularMovies= r;
+      }else{
+        this.moviesService.getPopularMovies();
+        }
+      }
+ 
 }
--- angular-popcorn v2/src/app/services/movies.service.ts
+++ angular-popcorn v2/src/app/services/movies.service.ts
@@ -2,9 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Genre, GenreList } from '../models/interfaces/genre.interface';
 import { Movie, MoviesPopularResponse } from '../models/interfaces/movies-popular.interface';
 
-const movieUrl = `${environment.apiBaseUrl}/movie`;
+const movieUrl = `${environment.apiBaseUrl}`;
 
 
 @Injectable({
@@ -15,11 +16,15 @@ export class MoviesService {
   constructor(private http: HttpClient) { }
 
   getPopularMovies(): Observable<MoviesPopularResponse> {
-    return this.http.get<MoviesPopularResponse>(`${movieUrl}/popular?api_key=${environment.apiKey}`);
+    return this.http.get<MoviesPopularResponse>(`${movieUrl}/movie/popular?api_key=${environment.apiKey}`);
   }
 
   getMovie(id: number): Observable<Movie>{
-    return this.http.get<Movie>(`${movieUrl}/${id}?api_key=${environment.apiKey}`);
+    return this.http.get<Movie>(`${movieUrl}/movie/${id}?api_key=${environment.apiKey}`);
+  }
+
+  getGenreList(): Observable<GenreList>{
+    return this.http.get<GenreList>(`${movieUrl}/genre/movie/list?api_key=${environment.apiKey}`);
   }
 
 }
