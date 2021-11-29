54924c04ba3a532cebc7ed24d6c955fec2dd6678
Francisco Javier Navarro Vega <navarro.vefra21@triana.salesianos.edu>
2021-11-29 13:17:36 +0100

    d

--- angular-minitwitter/src/app/components/register/register.component.ts
+++ angular-minitwitter/src/app/components/register/register.component.ts
@@ -11,6 +11,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class RegisterComponent implements OnInit {
 
+  
   registerForm = new FormGroup({
     username: new FormControl(''),
     email: new FormControl(''),
