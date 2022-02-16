8cedd91d35961b26a88d9da1e5addd2049ecd808
Francisco Javier Navarro Vega <navarro.vefra21@triana.salesianos.edu>
2022-02-16 12:21:21 +0100

    comiteo

--- flutter/flutter_miarmapp/lib/main.dart
+++ flutter/flutter_miarmapp/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_miarmapp/screens/login_screen.dart';
+import 'package:flutter_miarmapp/screens/login.dart';
 import 'package:flutter_miarmapp/screens/menu_screen.dart';
 import 'package:flutter_miarmapp/screens/register_screen.dart';
 
@@ -17,10 +17,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      initialRoute: '/login',
   routes: {
     '/': (context) => const MenuScreen(),
-    '/login': (context) => const LoginScreen(),
+    '/login': (context) => LoginPage(),
     '/register': (context) => const RegisterScreen(),
   },
     );
--- flutter/flutter_miarmapp/lib/screens/menu_screen.dart
+++ flutter/flutter_miarmapp/lib/screens/menu_screen.dart
@@ -1,58 +1,85 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_miarmapp/screens/home_screen.dart';
 import 'package:flutter_miarmapp/screens/profile_screen.dart';
 import 'package:flutter_miarmapp/screens/search_screen.dart';
-import 'package:flutter_miarmapp/widgets/home_app_bar.dart';
 
 class MenuScreen extends StatefulWidget {
-  const MenuScreen({ Key? key }) : super(key: key);
+  const MenuScreen({Key? key}) : super(key: key);
 
   @override
   _MenuScreenState createState() => _MenuScreenState();
 }
 
 class _MenuScreenState extends State<MenuScreen> {
-  int _selectedIndex = 0;
+  int _currentIndex = 0;
 
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    HomeScreen(),
-    SearchScreen(),
-    ProfileScreen(),
+  List<Widget> pages = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const ProfileScreen()
   ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: CupertinoTabBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+        body: pages[_currentIndex], bottomNavigationBar: _buildBottomBar());
+  }
+
+  Widget _buildBottomBar() {
+    return Container(
+        decoration: BoxDecoration(
+            border: const Border(
+          top: BorderSide(
+            color: Color(0xfff1f1f1),
+            width: 1.0,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/images/avatar.jpeg'),
-            label: 'My profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
+        )),
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              child: Icon(Icons.home,
+                  color: _currentIndex == 0
+                      ? Colors.black
+                      : const Color(0xff999999)),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 0;
+                });
+              },
+            ),
+            GestureDetector(
+              child: Icon(Icons.search,
+                  color: _currentIndex == 1
+                      ? Colors.black
+                      : const Color(0xff999999)),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 1;
+                });
+              },
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _currentIndex = 2;
+                });
+              },
+              child: Container(
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), border: Border.all(color: _currentIndex == 2? Colors.black: Colors.transparent, width: 1)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.asset(
+                      'assets/images/avatar.jpeg',
+                      width: 30,
+                    ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
